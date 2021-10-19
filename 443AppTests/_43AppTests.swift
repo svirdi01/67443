408bd19ae7de09482512ba3b3bf2261e61d03113
Simran Virdi <svirdi@andrew.cmu.edu>
2021-10-19 15:09:19 -0400

    Testing

---
+++ 443AppTests/_43AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  _43AppTests.swift
+//  443AppTests
+//
+//  Created by Simran Virdi on 10/19/21.
+//
+
+import XCTest
+@testable import _43App
+
+class _43AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
