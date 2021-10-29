6c7dbc447fc54b36c386634f59c060e779dd192f
svirdi01 <svirdi@andrew.cmu.edu>
2021-10-29 11:32:22 -0400

    made navigation bar

--- 443App/BottomBar.swift
+++ 443App/BottomBar.swift
@@ -10,12 +10,36 @@ import SwiftUI
 
 
 struct BottomBar: View {
+ 
   @ObservedObject var viewModel = ViewModel()
+  let viewController = ViewController()
   
   var body: some View{
-  HStack{
-    Text("BOTTOM BAR!!!!!!!!!!!!!!!!!!!")
-  }
+    TabView {
+       Text("Journal View")
+         .tabItem {
+            Image(systemName: "phone.fill")
+            Text("First Tab")
+          }
+      
+      MapView(viewController: viewController, viewModel: viewModel)
+           .tabItem {
+              Image(systemName: "tv.fill")
+              Text("Second Tab")
+            }
+      
+      Text("User View")
+           .tabItem {
+              Image(systemName: "tv.fill")
+              Text("Second Tab")
+            }
+      
+      
+      
+    }
+    
+    
+    
   }
 }
 
