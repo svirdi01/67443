5eabb01cafd81884fbf14df604d06e12fa534cbf
nehajoshi-ctrl <nehajosh@andrew.cmu.edu>
2021-11-04 22:41:29 -0400

    yes im on main, yes im writing tests, no they ar enot working

---
+++ 443AppTests/ViewTests.swift
@@ -0,0 +1,28 @@
+//
+//  ViewTests.swift
+//  443AppTests
+//
+//  Created by Neha Joshi on 11/4/21.
+//
+import XCTest
+import Foundation
+
+class ViewTests: XCTestCase {
+
+  func testExample(){
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    XCTAssertNil(nil)
+    
+  }
+
+}
+
--- 443AppTests/_43AppTests.swift
+++ 443AppTests/_43AppTests.swift
@@ -12,6 +12,7 @@ class _43AppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+     
     }
 
     override func tearDownWithError() throws {
