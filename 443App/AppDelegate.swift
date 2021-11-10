81812d1fd7a61ea1a79f1ecc5dd58f39f0dd2093
svirdi01 <svirdi@andrew.cmu.edu>
2021-11-09 21:15:31 -0500

    added. firebase

--- 443App/AppDelegate.swift
+++ 443App/AppDelegate.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   let location = Location()
+  
+  var window: UIWindow?
+
   
   func didDiscardSceneSessions(){
     location.loadLocation()
@@ -26,10 +30,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
-  }
+  func application(_ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      FirebaseApp.configure()
+
+      return true
+    }
 
   // MARK: UISceneSession Lifecycle
 
