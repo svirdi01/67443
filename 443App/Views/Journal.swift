eff58df80ba37e33998737121f78f59df4e727f0
claudia0sorio <39599257+claudia0sorio@users.noreply.github.com>
2021-11-27 23:39:31 -0500

    search

--- 443App/Views/Journal.swift
+++ 443App/Views/Journal.swift
@@ -4,24 +4,39 @@
 //
 //  Created by Claudia Osorio on 11/11/21.
 //
-
 import Foundation
 import SwiftUI
  
 struct Journal: View {
   //@EnvironmentObject var userPins: UserPins
   @ObservedObject var uvm: UserViewModel
-  
+  @State var searchField: String = ""
+  @State var displayedPins = [MemoryPin]()
   init(uvm: UserViewModel)
   {
     self.uvm = uvm
   }
   
   var body: some View {
+    
+    let binding = Binding<String>(get: {
+      self.searchField
+    }, set: {
+      self.searchField = $0
+      uvm.search(searchText: self.searchField)
+      self.displayPins()
+    })
+    
     NavigationView {
+      
+      
+      VStack{
+        TextField("Search:", text: binding).offset(x: 20)
+
+        if(searchField != ""){
 
       List {
-        ForEach(uvm.memoryPins)
+        ForEach(self.displayedPins)
         { pin in
           NavigationLink(destination: PinDetail(uvm: uvm, pin: pin))
           {
@@ -35,8 +50,45 @@ struct Journal: View {
             Image(systemName: "plus")
         }
       )
+        }
+        else{
+          List {
+            ForEach(uvm.memoryPins)
+            { pin in
+              NavigationLink(destination: PinDetail(uvm: uvm, pin: pin))
+              {
+                PinRow(pin: pin)
+              }
+            }
+          }
+          .navigationBarTitle("My Memories")
+          .navigationBarItems(trailing:
+            NavigationLink(destination: AddPin(uvm: uvm)) {
+                Image(systemName: "plus")
+            }
+          )
+        }
+        
+        
+      }
     }
+
+   
+    
   }
+  
+  func loadData() {
+    displayedPins = uvm.memoryPins
+  }
+  
+  
+    func displayPins() {
+      if searchField != "" {
+        displayedPins = uvm.filteredmemoryPins
+      }
+    }
+    
+
   
   
 }
