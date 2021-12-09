bec8aa70b10b9d3d8d6baae7f888fb39caac868a
svirdi01 <svirdi@andrew.cmu.edu>
2021-12-09 11:17:15 -0500

    fixed search

--- 443App/Views/LogIn.swift
+++ 443App/Views/LogIn.swift
@@ -17,8 +17,8 @@ struct LogIn: View {
   
   
   @State var isNavigationBarHidden: Bool = true
-  @State var emailField: String = "urmom@gmail.com"
-  @State var passField: String = "urmommy"
+  @State var emailField: String = ""
+  @State var passField: String = ""
   @ObservedObject var svm: AppViewModel
   @ObservedObject var uvm: UserViewModel
   let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
